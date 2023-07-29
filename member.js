c0e2da71b6eb467af74e068fb6a6b629d11cab52
nethminf <44412938+nethminf@users.noreply.github.com>
2023-07-29 11:57:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMemmber() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django'];
+    var member = {
+        name: 'Sergey',
+        age: 30,
+        skills: skills
+    };
+    return member.skills[3];
+}
